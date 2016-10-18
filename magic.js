079620afd6886ee5489a18bcb1122d3274fa5049
Nathan Friend <nathan.friend@gmail.com>
2016-10-18 13:05:51 +0100

    add fields and change css

--- magic.js
+++ magic.js
@@ -1,76 +1,56 @@
-// magic.js
-$(document).ready(function() {
+$(document).ready(function () {
+    $('form').submit(function (event) {
+        //reset errors
+        $('#name-group>input').removeClass('has-error');
+        $('#dob-group>input').removeClass('has-error');
+        $('#email-group>input').removeClass('has-error');
+        $('#country-group>input').removeClass('has-error');
 
-	// process the form
-	$('form').submit(function(event) {
+        // get form data
+        var formData = {
+            'name': $('input[name=name]').val(),
+            'email': $('input[name=email]').val(),
+            'dob': $('input[name=dob]').val(),
+            'country': $('input[name=country]').val()
+        };
 
-		$('.form-group').removeClass('has-error'); // remove the error class
-		$('.help-block').remove(); // remove the error text
+        // process the form
+        $.ajax({
+            type: 'POST',
+            url: 'process.php',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        })
+        
+                // done promise callback
+                .done(function (data) {
+                    // errors and validation messages
+                    if (!data.success) {
+                        if (data.errors.name) {
+                            $('#name-group>input').addClass('has-error');
+                        }
 
-		// get the form data
-		// there are many ways to get this data using jQuery (you can use the class or id also)
-		var formData = {
-			'name' 				: $('input[name=name]').val(),
-			'email' 			: $('input[name=email]').val(),
-			'superheroAlias' 	: $('input[name=superheroAlias]').val()
-		};
+                        if (data.errors.dob) {
+                            $('#dob-group>input').addClass('has-error');
+                        }
 
-		// process the form
-		$.ajax({
-			type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-			url 		: 'process.php', // the url where we want to POST
-			data 		: formData, // our data object
-			dataType 	: 'json', // what type of data do we expect back from the server
-			encode 		: true
-		})
-			// using the done promise callback
-			.done(function(data) {
-
-				// log data to the console so we can see
-				console.log(data); 
-
-				// here we will handle errors and validation messages
-				if ( ! data.success) {
-					
-					// handle errors for name ---------------
-					if (data.errors.name) {
-						$('#name-group').addClass('has-error'); // add the error class to show red input
-						$('#name-group').append('<div class="help-block">' + data.errors.name + '</div>'); // add the actual error message under our input
-					}
-
-					// handle errors for email ---------------
-					if (data.errors.email) {
-						$('#email-group').addClass('has-error'); // add the error class to show red input
-						$('#email-group').append('<div class="help-block">' + data.errors.email + '</div>'); // add the actual error message under our input
-					}
-
-					// handle errors for superhero alias ---------------
-					if (data.errors.superheroAlias) {
-						$('#superhero-group').addClass('has-error'); // add the error class to show red input
-						$('#superhero-group').append('<div class="help-block">' + data.errors.superheroAlias + '</div>'); // add the actual error message under our input
-					}
-
-				} else {
-
-					// ALL GOOD! just show the success message!
-					$('form').append('<div class="alert alert-success">' + data.message + '</div>');
-
-					// usually after form submission, you'll want to redirect
-					// window.location = '/thank-you'; // redirect a user to another page
-
-				}
-			})
-
-			// using the fail promise callback
-			.fail(function(data) {
-
-				// show any errors
-				// best to remove for production
-				console.log(data);
-			});
-
-		// stop the form from submitting the normal way and refreshing the page
-		event.preventDefault();
-	});
+                        if (data.errors.email) {
+                            $('#email-group>input').addClass('has-error');
+                        }
+                        if (data.errors.country) {
+                            $('#country-group>input').addClass('has-error');
+                        }
+                    } else {
+                        // Show the success message!
+                        $('form').html('<div class="alert alert-success">' + data.message + '</div>');
+                    }
+                })
+                // fail promise callback
+                .fail(function (data) {
+                });
+        // stop the form refreshing the page
+        event.preventDefault();
+    });
 
 });
